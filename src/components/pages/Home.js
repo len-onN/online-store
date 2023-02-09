455d395ace12839c7dd111c31e65a85daf2b7721
Pedro Henrique Alves Farias <pedro.alves.hf@Pedros-MacBook-Pro.local>
2023-02-09 17:26:00 -0300

    main-group-shopping-cart-3

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ShoppingCart from '../ShoppingCart';
 
 class Home extends React.Component {
   render() {
@@ -16,8 +18,13 @@ class Home extends React.Component {
           data-testid="home-initial-message"
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
-
         </h1>
+        <Link
+          data-testid="shopping-cart-button"
+          to="/shopping-cart"
+        >
+          Shopping Cart
+        </Link>
       </div>
     );
   }
