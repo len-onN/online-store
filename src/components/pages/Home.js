a3ba66e7d1fa1749799113ec260f477ccb3f9489
Pedro Henrique Alves Farias <pedro.alves.hf@Pedros-MacBook-Pro.local>
2023-02-09 16:45:22 -0300

    second task - empty doen

---
+++ src/components/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <label
+          htmlFor=""
+        >
+          <input
+            type="text"
+            name="search"
+          />
+        </label>
+        <h1
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
