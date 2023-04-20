614a70b9503664990ada8842b7c98bcdda67b9ac
Lenon <lenonphilippi@gmail.com>
2023-04-20 11:04:19 -0300

    botão pesquisar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -94,7 +94,7 @@ class Home extends Component {
             onClick={ this.handleButton }
           >
             {' '}
-            clica
+            Pesquisar
             {' '}
 
           </button>
